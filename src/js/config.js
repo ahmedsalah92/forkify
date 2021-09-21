eb00d74ae76d170b1e387d320566973f3e7c1dcb
ahmedsalah92 <ahmedsalaheid92@gmail.com>
2021-09-21 15:40:04 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8a555b08-36ec-4b02-b973-d70bc61fe8bc';
+export const MODAL_CLOSE_SEC = 2.5;
